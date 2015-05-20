408358e47e05fa1dd53f0da0d8e6cca4d61bc75f
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-05-20 09:46:46 -0400

    test punch

---
+++ blah.js
@@ -0,0 +1,21 @@
+
+var utp = require('./')
+
+var conn = utp.connect({
+  port: 12345,
+  address: '54.236.214.150',
+  localPort: 12345
+})
+
+conn.on('data', function(data) {
+  console.log('server', data.toString())
+})
+
+conn.on('error', console.log)
+
+var server = utp.createServer(function(c) {
+  c.on('data', function(data) {
+    console.log('client', data.toString())
+    c.write(data)
+  })
+}).listen(conn.socket)
